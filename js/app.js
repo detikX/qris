733a7dce2138129698b0f514f7fc66817888499d
dediwibisono17 <dedi.wibisono17@gmail.com>
2025-07-11 14:26:23 +0700

    jhhjhj

--- js/app.js
+++ js/app.js
@@ -3,3 +3,45 @@ $(document).ready(function () {
     // new WOW().init();
 })
 
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".box",
+        start: "center center",
+        end: "bottom top",
+        scrub: 1,
+        pin: true
+    }
+})
+    .from(".text1", {
+        // x: innerWidth * 1,
+        opacity: 0,
+
+    })
+    .from(".text2,.text1", {
+        // x: innerWidth * 1.8
+        opacity: 0,
+    })
+    .from(".text3,.text1,.text2", {
+        // x: innerWidth * 1.5
+        opacity: 0,
+    })
+    .from(".text4,.text2,.text3,.text1", {
+        // x: innerWidth * -1
+        opacity: 0,
+    })
+    .from(".text5,.text3", {
+        // x: innerWidth * -1.5
+        opacity: 0,
+    })
+    .from(".text6", {
+        // y: innerHeight * 1.5
+    })
+    .from(".angkax", {
+        y: innerHeight * 1,
+        rotate: 360
+    }).from(".angkax1", {
+        y: innerHeight * 1,
+
+    })
+
