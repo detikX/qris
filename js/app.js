0c875e35d7ec319815abf23de38d226097f6cad2
dediwibisono17 <dedi.wibisono17@gmail.com>
2025-07-16 12:49:03 +0700

    frf

--- js/app.js
+++ js/app.js
@@ -15,32 +15,36 @@ gsap.timeline({
     }
 })
     .from(".text1", {
-        // x: innerWidth * 1,
-        opacity: 0,
+        x: innerWidth * 1,
+        //opacity: 0,
 
     })
 
 
     .from(".text2", {
-        // x: innerWidth * 1.8
-        opacity: 0,
+        x: innerWidth * -1.8
+        //opacity: 0,
     })
 
     .from(".text3", {
-        // x: innerWidth * 1.5
-        opacity: 0,
+        x: innerWidth * 1.5
+        //opacity: 0,
     })
     .from(".text4", {
-        // x: innerWidth * -1
-        opacity: 0,
+        x: innerWidth * -1
+        //opacity: 0,
     })
     .from(".text5", {
-        // x: innerWidth * -1.5
-        opacity: 0,
+        x: innerWidth * -1.5
+        //opacity: 0,
     })
     .from(".text6", {
-        // x: innerWidth * -1.5
-        opacity: 0,
+        x: innerWidth * 2
+        //opacity: 0,
+    })
+    .from(".text7", {
+        x: innerWidth * -2
+        //opacity: 0,
     })
 
 
