525bd1d2da2ab80c6254f9b2ebad16fb7ea58e4a
dediwibisono17 <dedi.wibisono17@gmail.com>
2025-07-17 11:00:44 +0700

    sws

--- js/app.js
+++ js/app.js
@@ -48,3 +48,37 @@ gsap.timeline({
     })
 
 
+
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".box2",
+        start: "center center",
+        end: "bottom top",
+        scrub: 1,
+        pin: true,
+        // markers: true
+    }
+})
+    .from(".text-title1", {
+        x: innerWidth * 1,
+        //opacity: 0,
+
+    })
+
+
+    .from(".text-title2", {
+        x: innerWidth * -1.8
+        //opacity: 0,
+    })
+    .from(".gb-1", {
+        x: innerWidth * 1,
+        //opacity: 0,
+
+    })
+
+
+    .from(".gb-2", {
+        x: innerWidth * -1.8
+        //opacity: 0,
+    })
